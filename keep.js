d9f139bfe390912a587f7d3d6989b835ef5c01f8
mabinglei183 <42637658+mabinglei183@users.noreply.github.com>
2022-05-03 12:46:29 +0800

    Update keep.js

--- keep.js
+++ keep.js
@@ -1,19 +1,17 @@
 [rewrite_local]
 
 # keep 修改时间
-^https://api.gotokeep.com/snooker/v5/training-detail/\S url script-response-body https://raw.githubusercontent.com/mabinglei183/Script/main/keep.js
+^https:\/\/api\.gotokeep\.com\/snooker\/v5\/training-detail\/\S url script-response-body https://raw.githubusercontent.com/mabinglei183/Script/main/keep.js
 
 [mitm] 
 
 hostname = api.gotokeep.com
-*
-*
-*/
 
 
-let obj = JSON.parse($response.body);
+var obj = JSON.parse($response.body);
 
 obj.data.cardList.data.logInfo.duration=1234;
 obj.data.cardList.data.logInfo.calorie=43;
-obj.data.cardList.data.caloriePercentage" : 99.936170212765958;
+obj.data.cardList.data.caloriePercentage=99.936170212765958;
+
 $done({body: JSON.stringify(obj)});
