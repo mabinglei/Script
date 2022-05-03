9b8a479616a4da8e5e1003dd78e65fcd021aaa5b
mabinglei183 <42637658+mabinglei183@users.noreply.github.com>
2022-05-03 13:59:00 +0800

    Update keep.js

--- keep.js
+++ keep.js
@@ -14,6 +14,6 @@ var obj = JSON.parse($response.body);
 obj.data.cardList[1].data.logInfo.duration=1234;
 obj.data.cardList[1].data.logInfo.durationPercentage=99.317460317460316;
 obj.data.cardList[1].data.logInfo.calorie=43;
-obj.data.cardList[1].data.caloriePercentage=99.936170212765958;
+obj.data.cardList[1].data.logInfo.caloriePercentage=99.936170212765958;
 
 $done({body: JSON.stringify(obj)});
