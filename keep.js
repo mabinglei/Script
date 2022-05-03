f92644d5926be6d9518a237e6d8d07b3bbe9f701
mabinglei183 <42637658+mabinglei183@users.noreply.github.com>
2022-05-03 13:56:13 +0800

    Update keep.js

--- keep.js
+++ keep.js
@@ -12,6 +12,7 @@ hostname = api.gotokeep.com
 var obj = JSON.parse($response.body);
 
 obj.data.cardList[1].data.logInfo.duration=1234;
+obj.data.cardList[1].data.logInfo.durationPercentage=99.317460317460316;
 obj.data.cardList[1].data.logInfo.calorie=43;
 obj.data.cardList[1].data.caloriePercentage=99.936170212765958;
 
