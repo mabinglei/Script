819fb217e4243d6ee74516e25fae6935bc0f982b
mabinglei183 <42637658+mabinglei183@users.noreply.github.com>
2022-06-20 21:08:16 +0800

    Update keep.js

--- keep.js
+++ keep.js
@@ -13,10 +13,10 @@ var obj = JSON.parse($response.body);
 
 obj.data.cardList[1].data.logInfo.duration=1234;
 obj.data.cardList[1].data.logInfo.durationPercentage=99.317460317460316;
-obj.data.cardList[1].data.logInfo.calorie=110;
+obj.data.cardList[1].data.logInfo.calorie=170;
 obj.data.cardList[1].data.logInfo.caloriePercentage=99.936170212765958;
-obj.data.cardList[1].data.logInfo.endTimeText="2022\/06\/18 10:56";
-obj.data.cardList[1].data.logInfo.workoutFinishCount=2;
-obj.data.cardList[1].data.workoutInfo.workoutName="学生党暑假暴瘦！";
+obj.data.cardList[1].data.logInfo.endTimeText="2022\/06\/20 10:56";
+obj.data.cardList[1].data.logInfo.workoutFinishCount=1;
+obj.data.cardList[1].data.workoutInfo.workoutName="20分钟全程站立全身燃...";
 
 $done({body: JSON.stringify(obj)});
