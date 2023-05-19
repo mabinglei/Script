d3936cee9308baa5b12382b4839c318d233c5ee2
mabinglei <42637658+mabinglei@users.noreply.github.com>
2023-05-19 13:26:41 +0800

    Update chatgpt.js

--- chatgpt.js
+++ chatgpt.js
@@ -15,7 +15,7 @@ obj.accounts.default.entitlement = {
         "subscription_id" : null,
         "subscription_plan" : "chatgptplusplan",
         "has_active_subscription" : true
-      },
+      }
   
 obj.accounts.default.last_active_subscription = {
         "subscription_id" : null,
@@ -23,36 +23,4 @@ obj.accounts.default.last_active_subscription = {
         "purchase_origin_platform" : "openai"
       }
 
-body = JSON.stringify(obj);
-$done({body});
-
-
-// let obj = JSON.parse($response.body);
-// obj.data.hsjcsj=getTime();
-// obj.data.hsjcjgmc="云南迪安医学检验所有限公司"
-// obj.data.inDay7=true
-// obj.data.inDay14=true
-
-// function getTime() {
-//   let now = new Date();
-//   let today = now.getDate();
-//   let month = now.getMonth();
-//   let year = now.getFullYear();
-//   return year + "-" + (month+1) + "-" + today + " " + "08:15:30"
-// }
-
-// $done({body: JSON.stringify(obj)});
-// // {
-// //   "errcode": 0,
-// //   "errmsg": "SUCCESS",
-// //   "data": {
-// //     "hsjcjg": "阴性",
-// //     "hsjcsj": "2022-10-15 12:15:40",
-// //     "zjhm": "53**************12",
-// //     "hsjcjgmc": "寻甸县第一人民医院",
-// //     "hsjcjq": "阴性",
-// //     "xm": "马*磊",
-// //     "inDay7": true,
-// //     "inDay14": true
-// //   }
-// // }
+$done({body: JSON.stringify(obj)});
