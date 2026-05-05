c1cb93ee49e2cac5093b4d655ca7c86afecf31c3
Ma Binglei <42637658+mabinglei@users.noreply.github.com>
2026-05-05 15:24:45 +0800

    Update getCTCookie.js

--- Surge/JavaScript/getCTCookie.js
+++ Surge/JavaScript/getCTCookie.js
@@ -2,7 +2,7 @@ function getUrl() {
     if ($request.url.indexOf("https://e.dlife.cn/user/loginMiddle") !== -1) {
         const login_url = $request.url.match(/(http.+)&sign/)[1];
         $persistentStore.write(login_url, `china_telecom_loginUrl`);
-        $notification.post("写入天翼账号登录URL成功 🎉", "", $request.url, {
+        $notification.post("写入天翼账号登录URL成功 🎉", "", login_url, {
             "media-url": "https://raw.githubusercontent.com/anker1209/icon/main/zgdx.png",
             "auto-dismiss": 10
         })
@@ -12,7 +12,7 @@ function getUrl() {
 function getCookie() {
     const headerCookie = $response.headers["set-cookie"] || $response.headers["Set-Cookie"];
     $persistentStore.write(headerCookie, `china_telecom_cookie`);
-    $notification.post("写入天翼账号Cookie成功 🎉", "", $request.url, {
+    $notification.post("写入天翼账号Cookie成功 🎉", "", headerCookie, {
         "media-url": "https://raw.githubusercontent.com/anker1209/icon/main/zgdx.png",
         "auto-dismiss": 10
     })
